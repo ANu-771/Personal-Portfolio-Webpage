f2375946cca6a98fea5de86b07009c6d6719ad37
Anu <isuruanupama2021@gmail.com>
2026-03-22 16:22:37 +0530

    css and js add for pages

--- script.js
+++ script.js
@@ -13,3 +13,43 @@ navItemsMobile.forEach(item => {
         navMenu.classList.remove('active');
     });
 });
+
+
+//hero listin voice clip
+const voiceBtn = document.getElementById('voiceBtn');
+const voiceAudio = document.getElementById('voiceAudio');
+
+if (voiceBtn && voiceAudio) {
+    voiceBtn.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        if (voiceAudio.paused) {
+            voiceAudio.play();
+        } else {
+            voiceAudio.pause();
+        }
+    });
+}
+
+// --- Tab Switching tabs for Projects/Assignments ---
+function switchTab(tabName) {
+    const tabProjects = document.getElementById('tab-projects');
+    const tabAssignments = document.getElementById('tab-assignments');
+    const gridProjects = document.getElementById('grid-projects');
+    const gridAssignments = document.getElementById('grid-assignments');
+
+    if (tabName === 'projects') {
+        gridProjects.style.display = 'flex'; 
+        gridAssignments.style.display = 'none';
+
+        tabProjects.classList.add('active');
+        tabAssignments.classList.remove('active');
+        
+    } else if (tabName === 'assignments') {
+        gridProjects.style.display = 'none';
+        gridAssignments.style.display = 'grid'; 
+
+        tabAssignments.classList.add('active');
+        tabProjects.classList.remove('active');
+    }
+}
